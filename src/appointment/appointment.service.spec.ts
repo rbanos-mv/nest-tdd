8ef5848745c8592504bc89553789c5d6376242b5
Roberto A. Baños Alvarez <roberto.banos@gmail.com>
2025-04-29 02:10:43 +0000

    feat(appointment): ensure an unconfirmed appointment is created on success

--- src/appointment/appointment.service.spec.ts
+++ src/appointment/appointment.service.spec.ts
@@ -15,4 +15,21 @@ describe('AppointmentService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should schedule an unconfirmed appointment for a user on success', async () => {
+    const startTime = new Date('2022-01-01T14:00:00Z');
+    const endTime = new Date('2022-01-01T15:00:00Z');
+    const newAppointment = await service.scheduleAppointment({
+      patientId: 1,
+      startTime,
+      endTime,
+    });
+
+    expect(newAppointment).toEqual({
+      patientId: 1,
+      startTime,
+      endTime,
+      confirmed: false,
+    });
+  });
 });
--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Appointment } from './appointment.model';
+
+export interface AppointmentInput {
+  patientId: number;
+  startTime: Date;
+  endTime: Date;
+}
 
 @Injectable()
-export class AppointmentService {}
+export class AppointmentService {
+  public async scheduleAppointment(
+    appointmentData: AppointmentInput,
+  ): Promise<Appointment> {
+    return Promise.resolve({
+      ...appointmentData,
+      confirmed: false,
+    });
+  }
+}
