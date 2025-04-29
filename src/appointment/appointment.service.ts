0bd7a88924fa6e4b45f898607e13721162be0023
Roberto A. Baños Alvarez <roberto.banos@gmail.com>
2025-04-29 02:22:04 +0000

    feat(appointment): ensure an error is thrown when end time is before start time

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -12,6 +12,10 @@ export class AppointmentService {
   public async scheduleAppointment(
     appointmentData: AppointmentInput,
   ): Promise<Appointment> {
+    if (appointmentData.endTime < appointmentData.startTime) {
+      throw new Error("appointment's endTime should be after startTime");
+    }
+
     return Promise.resolve({
       ...appointmentData,
       confirmed: false,
