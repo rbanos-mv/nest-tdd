98bfc414106625614ef317680fa95030b61041a1
Roberto A. Baños Alvarez <roberto.banos@gmail.com>
2025-04-29 03:28:13 +0000

    feat(patient): ensure doesPatientExist method returns true

--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -7,16 +7,22 @@ export interface PatientInput {
 
 @Injectable()
 export class PatientService {
+  private readonly patients: Patient[] = [];
+
   public async register(patientInput: PatientInput): Promise<Patient> {
     const newPatient: Patient = {
       id: 1,
       name: patientInput.name,
     };
 
+    this.patients.push(newPatient);
+
     return Promise.resolve(newPatient);
   }
 
   public async doesPatientExist(patientId: number): Promise<boolean> {
-    return Promise.resolve(patientId != patientId);
+    return Promise.resolve(
+      this.patients.some((patient) => patient.id === patientId),
+    );
   }
 }
