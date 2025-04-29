372d4db13e739429729f23a46b2727dde79d72d1
Roberto A. Baños Alvarez <roberto.banos@gmail.com>
2025-04-29 02:49:47 +0000

    feat(patient): create patient module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppointmentModule } from './appointment/appointment.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [AppointmentModule],
+  imports: [AppointmentModule, PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })
