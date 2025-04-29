6ce399267fcf06657434bcc0ebf9fcb53eeda19d
Roberto A. Baños Alvarez <roberto.banos@gmail.com>
2025-04-29 02:30:13 +0000

    feat(appointment): ensure an error is thrown when end time is equal to start time

--- src/appointment/appointment.service.spec.ts
+++ src/appointment/appointment.service.spec.ts
@@ -45,4 +45,17 @@ describe('AppointmentService', () => {
       }),
     ).rejects.toThrow("appointment's endTime should be after startTime");
   });
+
+  it('should throw an error when end time is equal to start time', async () => {
+    const startTime = new Date('2022-01-01T14:00:00Z');
+    const endTime = startTime;
+
+    await expect(
+      service.scheduleAppointment({
+        patientId: 1,
+        startTime,
+        endTime,
+      }),
+    ).rejects.toThrow("appointment's endTime should be after startTime");
+  });
 });
--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -12,7 +12,7 @@ export class AppointmentService {
   public async scheduleAppointment(
     appointmentData: AppointmentInput,
   ): Promise<Appointment> {
-    if (appointmentData.endTime < appointmentData.startTime) {
+    if (appointmentData.endTime <= appointmentData.startTime) {
       throw new Error("appointment's endTime should be after startTime");
     }
 
